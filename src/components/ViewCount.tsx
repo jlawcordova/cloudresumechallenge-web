0c8216272525137a2c208f955b56e62bd742a721
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-02-27 17:45:38 +0800

    feat: retrieve view count from API

--- src/components/ViewCount.tsx
+++ src/components/ViewCount.tsx
@@ -1,21 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
 import "./ViewCount.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function ViewCount() {
-  const viewCount = 132;
-  const fomattedViewCount = new Intl.NumberFormat(
-    'en-US', {maximumSignificantDigits: 3, notation: "compact"}
-  ).format(viewCount);
+  const [viewCount, setViewCount] = useState<number>();
+  
+  useEffect(() => {
+    const addViewCount = async () => {
+      const axiosInstance = axios.create({
+        baseURL: 'https://api-resume.jlawcordova.com/',
+        timeout: 5000,
+      });
+
+      const response = await axiosInstance.patch("/view-count");
+  
+      setViewCount(Number(response.data.viewCount));
+    };
+
+    addViewCount();
+  }, []);
+
+  const compact = (i: number) => {
+    return new Intl.NumberFormat(
+      'en-US', {maximumSignificantDigits: 3, notation: "compact"}
+    ).format(i);
+  }
 
   return (
     <div className="container view-count-container">
-      <span>
-        <FontAwesomeIcon icon={faChartSimple}/>
-        <span className="view-count">
-          {fomattedViewCount} views
+      { viewCount == null ? <></> :
+        <span>
+          <FontAwesomeIcon icon={faChartSimple}/>
+          <span className="view-count">
+            {compact(viewCount)} views
+          </span>
         </span>
-      </span>
+      }
     </div>
   );
 }
