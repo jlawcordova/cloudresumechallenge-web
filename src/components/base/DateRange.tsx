5ba933fabfb6a4f6f085facd0c03dd39d21b6e82
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-03-04 11:28:44 +0800

    fix: remove space for no month date range

--- src/components/base/DateRange.tsx
+++ src/components/base/DateRange.tsx
@@ -5,18 +5,27 @@ interface IDateRangeProps {
 
 export default function DateRange({startDate, endDate}: IDateRangeProps) {
   // Only show the months for the recent 2 years.
+  // The year is always displayed.
   const start = <>
     {startDate == null
       || new Date().getFullYear() - startDate?.getFullYear() > 2 ?
-      "" : new Intl.DateTimeFormat('en', { month: 'short' }).format(startDate)}
-      &nbsp;
+      "" : new Intl.DateTimeFormat('en', { month: 'short' }).format(startDate)
+      + " "}
+
     {new Intl.DateTimeFormat('en', { year: 'numeric' }).format(startDate)}
   </>
+
+  // Similarly, the end date should
+  // only show the months for the recent 2 years. 
+  // The year is always displayed.
+  // It is assumed that the date is ongoing at the present time
+  // if no end date is provided.
   const end = endDate == null ? <>Present</> : <>
     {endDate == null
     || new Date().getFullYear() - endDate?.getFullYear() > 2 ?
-      "" : new Intl.DateTimeFormat('en', { month: 'short' }).format(endDate)}
-      &nbsp;
+      "" : new Intl.DateTimeFormat('en', { month: 'short' }).format(endDate)
+      + " "}
+
     {new Intl.DateTimeFormat('en', { year: 'numeric' }).format(endDate)}
   </>
 
